65b9c1d85279aa7b5c26dffab6c84f89b1e59fcb
ammoriddin <hakimjavoh@gmail.com>
2024-07-19 17:09:38 +0500

    FIX: Navbar Display

--- src/containers/navbar/inde.jsx
+++ src/containers/navbar/inde.jsx
@@ -1,28 +1,26 @@
 "use client"
 
-import useUserRegister from '@/hooks/useUserRegister/useUserRegister'
 import { Icons } from '@/icons'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
-  const pathname = usePathname()
-  useUserRegister()
+  const pathname = usePathname();
+
+  // Check if the pathname starts with /login or /register
+  if (pathname.startsWith('/login') || pathname.startsWith('/register')) {
+    return null;
+  }
 
   return (
-    <>
-      {
-        pathname !== "/login" || "/register" && 
-        <nav className='py-[17px] border-b-[1px] border-borderColor border-solid px-[20px] min-w-[1218px] flex justify-between '>
-          {/* logo */}
-          <Link href={'/'} className='flex gap-[10px] items-center'>
-            <Icons.logo className="w-[32px] h-[32px]" />
-            <h1 className='text-black text-[1.5rem] font-[800] leading-normal font-onest'>onest</h1>
-          </Link>
-        </nav>
-      }
-    </>
+    <nav className='py-[17px] border-b-[1px] border-borderColor border-solid px-[20px] min-w-[1218px] flex justify-between '>
+      {/* logo */}
+      <Link href={'/'} className='flex gap-[10px] items-center'>
+        <Icons.logo className="w-[32px] h-[32px]" />
+        <h1 className='text-black text-[1.5rem] font-[800] leading-normal font-onest'>onest</h1>
+      </Link>
+    </nav>
   )
 }
 
