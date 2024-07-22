431454c2d8953ba79c776fb69f3749ca872cd978
ammoriddin <hakimjavoh@gmail.com>
2024-07-22 14:00:05 +0500

    ADD: Coment Section

--- src/icons/index.js
+++ src/icons/index.js
@@ -699,5 +699,53 @@ export const Icons = {
                 fill="inherit"
             />
         </svg>
+    ),
+    smile: (props) => (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none"
+            {...props}
+        >
+            <path 
+                d="M16 13H14C14 14.1046 13.1046 15 12 15C10.8954 15 10 14.1046 10 13H8C8 15.2091 9.79086 17 12 17C14.2091 17 16 15.2091 16 13Z" 
+                fill="inherit"
+            />
+            <path 
+                d="M10 10C10 10.5523 9.55228 11 9 11C8.44772 11 8 10.5523 8 10C8 9.44771 8.44772 9 9 9C9.55228 9 10 9.44771 10 10Z" 
+                fill="inherit"
+            />
+            <path 
+                d="M15 11C15.5523 11 16 10.5523 16 10C16 9.44771 15.5523 9 15 9C14.4477 9 14 9.44771 14 10C14 10.5523 14.4477 11 15 11Z" 
+                fill="inherit"
+            />
+            <path 
+                fill-rule="evenodd" 
+                clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12Z" 
+                fill="inherit"
+            />
+        </svg>
+    ),
+    send: (props) => (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="34" 
+            height="34" 
+            viewBox="0 0 34 34" 
+            fill="none"
+            {...props}
+        >
+            <g clip-path="url(#clip0_46_3419)">
+                <path d="M31.1419 16.9706H15.5856" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M31.1421 16.9706L12.0503 26.1629L15.5858 16.9706L12.0503 7.77817L31.1421 16.9706Z" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_46_3419">
+                <rect width="24" height="24" fill="white" transform="translate(17) rotate(45)"/>
+                </clipPath>
+            </defs>
+        </svg>
     )
 }
