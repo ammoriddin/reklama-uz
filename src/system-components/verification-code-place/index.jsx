b76ed958951bdfe1a098b4a74ecc890c7ac1faa7
ammoriddin <hakimjavoh@gmail.com>
2024-07-25 15:01:39 +0500

    FIX: Deployment error

--- src/system-components/verification-code-place/index.jsx
+++ src/system-components/verification-code-place/index.jsx
@@ -12,14 +12,18 @@ const VerificationCodeInput = ({ length = 4 }) => {
     setCode(newCode);
 
     if (value && index < length - 1) {
-      document.getElementById(`code-input-${index + 1}`).focus();
+      if (typeof document !== 'undefined') {
+        document.getElementById(`code-input-${index + 1}`).focus();
+      }
     }
   };
 
   const handleKeyDown = (e, index) => {
     if (e.key === 'Backspace' && code[index] === '') {
       if (index > 0) {
-        document.getElementById(`code-input-${index - 1}`).focus();
+        if (typeof document !== 'undefined') {
+          document.getElementById(`code-input-${index - 1}`).focus();
+        }
       }
     }
   };
